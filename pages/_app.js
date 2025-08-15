5460cc548434815f7cf6f4aacc522eb78a46f92e
hunM4P <younghun4218@gmail.com>
2025-08-15 17:17:15 +0900

    메타픽셀 코드 삽입

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,48 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      if (typeof window !== "undefined" && typeof window.fbq === "function") {
+        window.fbq("track", "PageView");
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '816130357435841');
+        fbq('track', 'PageView');`}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=816130357435841&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
       <Component {...pageProps} />
       <Analytics />
     </>
